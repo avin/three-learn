5eb2ffb87701cd415c62d6ab70ed53d57e5786eb
Avin Grape <carcinogen75@gmail.com>
2017-02-24 19:51:41 +0300

    simple animation based on redux store value

--- src/containers/pages/simple-scene/index.js
+++ src/containers/pages/simple-scene/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import * as THREE from 'three';
+import Counter from '../counter'
 
 export class SimpleScene extends React.Component {
+
+    //newCubePosition = 0;
+
+    shouldComponentUpdate(nextProps, nextState){
+        //this.newCubePosition = nextProps.value;
+
+        return false;
+    }
+
     componentDidMount() {
         const canvasEl = this.refs.canvas;
         const width = canvasEl.width;
@@ -19,16 +29,24 @@ export class SimpleScene extends React.Component {
 
         const geometry = new THREE.BoxGeometry(1, 1, 1);
         const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-        const cube = new THREE.Mesh(geometry, material);
+        const cube = this.cube = new THREE.Mesh(geometry, material);
         scene.add(cube);
 
         camera.position.z = 5;
 
-        const render = function () {
+        const render = () => {
             requestAnimationFrame(render);
 
             cube.rotation.x += 0.1;
             cube.rotation.y += 0.1;
+
+            if (cube.position.y <= this.props.value){
+                cube.position.y = cube.position.y + 0.1
+            }
+
+            if (cube.position.y >= this.props.value){
+                cube.position.y = cube.position.y - 0.1
+            }
 
             renderer.render(scene, camera);
         };
@@ -36,9 +54,12 @@ export class SimpleScene extends React.Component {
         render();
     }
 
+    component
+
     render() {
         return (
             <div>
+                <Counter />
                 <canvas ref="canvas" width={800} height={600}/>
             </div>
         )
@@ -46,7 +67,10 @@ export class SimpleScene extends React.Component {
 }
 
 function mapStateToProps(state, ownProps) {
-    return {}
+    const value = state.counter.get('value');
+    return {
+        value
+    }
 }
 
 export default connect(
